3999bfb6c6ab086cb7c5dd3fe2763ab8f5ca8055
romuloaugusto <romuloaugusto.silva@gmail.com>
2018-10-15 00:11:59 -0300

    inital commit

---
+++ todo-app/frontend/src/main/App.js
@@ -0,0 +1,13 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import Routes from './routes'
+import Menu from '../template/menu'
+
+export default props => (
+  <div className='container' >
+    <Menu />
+    <Routes />
+  </div>
+)
+
+
---
+++ todo-app/frontend/src/todo/todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+
+}
+
+
